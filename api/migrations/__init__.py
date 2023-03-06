15c83aee319a2ed6eba616473ddb7cd1aed32765
Luane Mendonça Da Silva <luane.mendonca@payface.com.br>
2023-03-06 10:33:27 -0300

    CRUD

--- api/migrations/__init__.py
+++
@@ -1,4 +0,0 @@
-from .models import User
-from .models import Company
-from .models import Document
-
--- api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Company
-
-def company_list(request):
-    companies = Company.objects.all()
-    return render(request, 'company_list.html', {'companies': companies})
